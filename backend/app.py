f22b3aa3663e19631a84bb160701648e88a50d5f
Monika <monikaravikumar227@gmail.com>
2026-03-30 12:28:15 +0530

    Mysql service is added

--- backend/app.py
+++ backend/app.py
@@ -90,13 +90,11 @@ def index():
     return {"message": "Online Voting System API is running"}
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
 
 
     print("\n===== REGISTERED ROUTES =====")
     for rule in app.url_map.iter_rules():
         print(rule)
 
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
     
